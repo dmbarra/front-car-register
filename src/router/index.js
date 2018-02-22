0c45b34e817fc98f0fce05675c0ca139249e7d3c
Daniel Barra <dmbarra@gmail.com>
2018-02-22 11:59:59 +0000

    Adciona componente Menu

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
 import HelloWorld from '@/components/HelloWorld'
+import Menu from '@/components/Menu'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/menu',
+      name: 'Menu',
+      component: Menu
     }
   ]
 })
